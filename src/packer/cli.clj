93963c8348a41506a0a803ffd747f170eeb0fecf
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-11-23 08:58:56 -0200

    Start creating a CLI program.

--- src/packer/cli.clj
+++ src/packer/cli.clj
@@ -1,17 +1,31 @@
 (ns packer.cli
   (:gen-class)
   (:require [clojure.java.io :as io]
-            [packer.misc :as misc]))
+            [packer.api :as api]
+            [clojure.tools.cli :as tools.cli]
+            [packer.misc :as misc]
+            [clojure.string :as string]))
 
 (def ^:private cwd (io/file "."))
 
-(defn- file-or-dir-must-exist
-  [option-name]
-  [misc/file-exists? (format "%s: no such file or directory" option-name)])
+(def ^:private file-or-dir-must-exist
+  [misc/file-exists? "no such file or directory"])
+
+(def ^:private source-must-exist
+  [#(misc/file-exists? (:source %)) "no such file or directory"])
 
 (defn- accumulate
   [m k v]
   (update-in m [k] (fnil conj #{}) v))
+
+(defn- parse-extra-file
+  [value]
+  (let [source+target (string/split value #"\s*:\s*")]
+    (if (= 2 (count source+target))
+      (zipmap [:source :target] (map io/file source+target))
+      (throw (IllegalArgumentException.
+              "Invalid extra-file format. Please, specify extra
+    files in the form source:target")))))
 
 (def help
   ["?" "--help"
@@ -19,49 +33,57 @@
    :desc "show this help and exit"])
 
 (defn- parse-command
-  [program [command & args]]
+  [{:keys [fn options]} args]
   (tools.cli/parse-opts args
-                        (cons help (get program command))))
+                        (cons help options)))
 
 (def packer
   {"containerize"
    {:desc "Turns an input jar into a lightweight
   container according to provided options"
-    :opts [["-a" "--app-root PATH"
-            :id :app-root
-            :desc "app root of the container image. Classes and
+    :fn api/containerize
+    :options [["-a" "--app-root PATH"
+               :id :app-root
+               :desc "app root of the container image. Classes and
                       resource files will be copied to relative paths to the app
                       root."
-            :default (io/file "/app")
-            :parse-fn io/file]
-           ["-e" "--extra-file PATH"
-            :id :extra-files
-            :desc "extra files to be copied to the container
+               :default (io/file "/app")
+               :parse-fn io/file]
+              ["-e" "--extra-file PATH"
+               :id :extra-files
+               :desc "extra files to be copied to the container
                       image. The value must be passed in the form source:target
                       and this option can be repeated many times"
-            :assoc-fn accumulate]
-           ["-i" "--input JAR"
-            :id :jar-file
-            :desc "jar file to be containerized"
-            :parse-fn io/file
-            :validate (file-or-dir-must-exist "input")]
-           ["-m" "--manifest MANIFEST"
-            :id :manifest
-            :desc "manifest file describing the image to be built"
-            :parse-fn io/file
-            :validate (file-or-dir-must-exist "manifest")]
-           ["-p" "--project-root PATH"
-            :id :project-root
-            :desc "root dir of the project containing the source
+               :parse-fn parse-extra-file
+               :validate source-must-exist
+               :assoc-fn accumulate]
+              ["-i" "--input JAR"
+               :id :jar-file
+               :desc "jar file to be containerized"
+               :parse-fn io/file
+               :validate file-or-dir-must-exist]
+              ["-I" "--internal-deps REGEX"
+               :id :internal-deps-re
+               :desc "java regex to determine internal dependencies. Can be
+            repeated many times for a logical or"
+               :parse-fn re-pattern
+               :assoc-fn accumulate]
+              ["-m" "--manifest MANIFEST"
+               :id :manifest
+               :desc "manifest file describing the image to be built"
+               :parse-fn io/file
+               :validate file-or-dir-must-exist
+               :assoc-fn #(assoc %1 %2 (misc/read-json %3))]
+              ["-p" "--project-root PATH"
+               :id :project-root
+               :desc "root dir of the project containing the source
                       code. Packer inspects the project to obtain some useful
                       insights about how to organize layers."
-            :default cwd
-            :parse-fn io/file
-            :validate (file-or-dir-must-exist "project-root")]
-           ["-o" "--output PATH"
-            :id :tarball
-            :desc "path to save the tarball containing the built image"
-            :default (io/file "image.tar")
-            :parse-fn io/file]]}})
-
-(parse-command packer ["containerize"])
+               :default cwd
+               :parse-fn io/file
+               :validate file-or-dir-must-exist]
+              ["-o" "--output PATH"
+               :id :tarball
+               :desc "path to save the tarball containing the built image"
+               :default (io/file "image.tar")
+               :parse-fn io/file]]}})
