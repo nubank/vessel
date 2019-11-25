2789aa1f5fcd27d1853c6a960b09a50ab69cd48f
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-11-25 10:20:51 -0200

    Show the elapsed time at the end of the containerization.

--- src/packer/api.clj
+++ src/packer/api.clj
@@ -6,11 +6,22 @@
   (:import java.io.File
            [java.util.jar JarEntry JarFile]))
 
+(defmacro ^:private with-elapsed-time
+  "Evaluates body and shows a log message by displaying the elapsed time in the process.
+
+  Returns whatever body yelds."
+  [& body]
+  `(let [start#  (misc/now)
+         result# (do ~@body)]
+     (misc/log :info "packer" "done in %s"
+               (misc/duration->string (misc/duration-between start# (misc/now))))
+     result#))
+
 (defn- get-project-insights
   "List project sources and resources in order to obtain insights about
   how image layers must be organized."
   [^File project-root]
-  {:known-sources (misc/find-files-at (io/file project-root "src"))
+  {:known-sources   (misc/find-files-at (io/file project-root "src"))
    :known-resources (misc/find-files-at (io/file project-root "resources"))})
 
 (defn- unpack-jar
@@ -35,10 +46,11 @@
 (defn containerize
   "Turns an input jar into a lightweight container according to the
   provided options."
-  [{:keys [jar-file project-root] :as context}]
+  [{:keys [^File jar-file ^File project-root] :as context}]
   {:pre [jar-file project-root]}
-  (with-clean-dir [dest-dir (io/file ".packer")]
-    (let [files (unpack-jar jar-file dest-dir)]
+  (with-elapsed-time
+    (let [files (with-clean-dir [dest-dir (io/file ".packer")]
+                  (unpack-jar jar-file dest-dir))]
       (-> (into context (get-project-insights project-root))
           (assoc :files files)
           image/render-build-plan
