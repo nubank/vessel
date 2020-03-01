ce7ec14985e2a02d272ef1d73f08ceccb1c2b4d8
Alan Ghelardi <alan.ghelardi@gmail.com>
2020-03-01 19:07:55 -0300

    wip: start writing integration tests.

---
+++ test/integration/vessel/program_integration_test.clj
@@ -0,0 +1,66 @@
+(ns vessel.program-integration-test
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [mockfn.macros :refer [calling providing]]
+            [vessel.misc :as misc]
+            [vessel.program :as vessel]
+            [vessel.test-helpers :refer [ensure-clean-test-dir]]))
+
+(use-fixtures :once (ensure-clean-test-dir))
+
+(def working-dir (io/file "target/tests/program-integration-test"))
+
+(deftest vessel-test
+  (providing [(#'vessel/exit int?) (calling identity)]
+
+             (testing "generates a manifest file containing metadata about the
+               application in question"
+               (is (zero? (vessel/-main "manifest"
+                                        "--attribute" "name:my-app"
+                                        "--attribute" "git-commit:07dccc801700cbe28e4a428e455b4627e0ab4ba9"
+                                        "--object" "service"
+                                        "--output" (str (io/file working-dir "my-app.json")))))
+
+               (is (= {:service {:name       "my-app"
+                                 :git-commit "07dccc801700cbe28e4a428e455b4627e0ab4ba9"}}
+                      (misc/read-json (io/file working-dir "my-app.json")))))
+
+             (testing "generates a manifest file describing the base image"
+               (is (zero? (vessel/-main "image"
+                                        "--repository" "openjdk"
+                                        "--tag" "alpine"
+                                        "--attribute" "comment:OpenJDK Alpine image"
+                                        "--output" (str (io/file working-dir "openjdk-alpine.json")))))
+
+               (is (= {:image
+                       {:repository "openjdk"
+                        :registry   "docker.io"
+                        :tag        "alpine"
+                        :comment    "OpenJDK Alpine image"}}
+                      (misc/read-json (io/file working-dir "openjdk-alpine.json")))))
+
+             (testing "generates a manifest file describing the application's
+               image to be built; merges the manifests created previously"
+               (is (zero? (vessel/-main "image"
+                                        "--repository" "nubank/my-app"
+                                        "--registry" "localhost:5000"
+                                        "--attribute" "comment:My Clojure application"
+                                        "--base-image" (str (io/file working-dir "openjdk-alpine.json"))
+                                        "--merge-into" (str (io/file working-dir "my-app.json"))
+                                        "--output" (str (io/file working-dir "image.json")))))
+
+               (is (= {:image
+                       {:repository "nubank/my-app"
+                        :registry   "localhost:5000"
+                        :comment    "My Clojure application"
+                        :tag        "71237936c573fd34cde3a0dea637149be73a5323c7dbe16e1b119d36f51cffe4"}
+                       :base-image
+                       {:image
+                        {:repository "openjdk"
+                         :registry   "docker.io"
+                         :comment    "OpenJDK Alpine image"
+                         :tag        "alpine"}}
+                       :service
+                       {:name       "my-app"
+                        :git-commit "07dccc801700cbe28e4a428e455b4627e0ab4ba9"}}
+                      (misc/read-json (io/file working-dir "image.json")))))))
