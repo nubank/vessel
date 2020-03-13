097be0c35cff6612b0f11aa05b0c21e89d6b92c4
Alan Ghelardi <alan.ghelardi@gmail.com>
2020-03-13 08:48:25 -0300

    Use misc/make-empty-dir and add classpath function.

--- test/vessel/test_helpers.clj
+++ test/vessel/test_helpers.clj
@@ -1,17 +1,9 @@
 (ns vessel.test-helpers
   (:require [clojure.java.io :as io]
-            [clojure.string :as string])
+            [clojure.java.shell :as shell]
+            [clojure.string :as string]
+            [vessel.misc :as misc])
   (:import java.io.File))
-
-(defn cleanup-dir
-  [^File dir]
-  (->> (io/file "target")
-       file-seq
-       reverse
-       (run! (fn [file]
-               (.delete file))))
-  (io/make-parents dir)
-  (.mkdir dir))
 
 (defmacro ensure-clean-test-dir
   "Expands to a fixture function that creates a clean directory under target/tests/<namespace>.
@@ -21,6 +13,23 @@
   target/tests/my-feature-test before each test in this namespace."
   []
   `(fn [test#]
-     (let [dir# (io/file "target" "tests" ~(last (string/split (name (ns-name *ns*)) #"\.")))]
-       (cleanup-dir dir#)
-       (test#))))
+     (misc/make-empty-dir "target" "tests" ~(last (string/split (name (ns-name *ns*)) #"\.")))
+     (test#)))
+
+(defn ^String classpath
+  "Runs the clojure -Spath command to determine the classpath of the
+  project at working-dir."
+  [^File working-dir]
+  (let [{:keys [exit err out]}
+        (shell/sh "clojure" "-Spath"
+                  :dir working-dir
+                  :env (into {} (System/getenv)))]
+    (if (zero? exit)
+      (->> (string/split (string/trim out) #":")
+           (map (fn [path]
+                  (if (.isAbsolute (io/file path))
+                    path
+                    (str (io/file (.getAbsoluteFile working-dir) path)))))
+           (string/join ":"))
+      (throw (ex-info "clojure -Spath failed"
+                      {:process-output err})))))
