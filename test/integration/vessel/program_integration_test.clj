d3e8d09248c8a76acc1b5be35a68984ac81571a7
Alan Ghelardi <alan.ghelardi@gmail.com>
2020-03-13 11:43:47 -0300

    Complete integration tests.

--- test/integration/vessel/program_integration_test.clj
+++ test/integration/vessel/program_integration_test.clj
@@ -4,11 +4,13 @@
             [mockfn.macros :refer [calling providing]]
             [vessel.misc :as misc]
             [vessel.program :as vessel]
-            [vessel.test-helpers :refer [ensure-clean-test-dir]]))
+            [vessel.test-helpers :refer [classpath ensure-clean-test-dir]]))
 
 (use-fixtures :once (ensure-clean-test-dir))
 
-(def working-dir (io/file "target/tests/program-integration-test"))
+(def project-dir (io/file "test/resources/my-app"))
+
+(def target-dir (io/file "target/tests/program-integration-test"))
 
 (deftest vessel-test
   (providing [(#'vessel/exit int?) (calling identity)]
@@ -19,25 +21,25 @@
                                         "--attribute" "name:my-app"
                                         "--attribute" "git-commit:07dccc801700cbe28e4a428e455b4627e0ab4ba9"
                                         "--object" "service"
-                                        "--output" (str (io/file working-dir "my-app.json")))))
+                                        "--output" (str (io/file target-dir "my-app.json")))))
 
                (is (= {:service {:name       "my-app"
                                  :git-commit "07dccc801700cbe28e4a428e455b4627e0ab4ba9"}}
-                      (misc/read-json (io/file working-dir "my-app.json")))))
+                      (misc/read-json (io/file target-dir "my-app.json")))))
 
              (testing "generates a manifest file describing the base image"
                (is (zero? (vessel/-main "image"
                                         "--repository" "openjdk"
                                         "--tag" "alpine"
                                         "--attribute" "comment:OpenJDK Alpine image"
-                                        "--output" (str (io/file working-dir "openjdk-alpine.json")))))
+                                        "--output" (str (io/file target-dir "openjdk-alpine.json")))))
 
                (is (= {:image
                        {:repository "openjdk"
                         :registry   "docker.io"
                         :tag        "alpine"
                         :comment    "OpenJDK Alpine image"}}
-                      (misc/read-json (io/file working-dir "openjdk-alpine.json")))))
+                      (misc/read-json (io/file target-dir "openjdk-alpine.json")))))
 
              (testing "generates a manifest file describing the application's
                image to be built; merges the manifests created previously"
@@ -45,9 +47,9 @@
                                         "--repository" "nubank/my-app"
                                         "--registry" "localhost:5000"
                                         "--attribute" "comment:My Clojure application"
-                                        "--base-image" (str (io/file working-dir "openjdk-alpine.json"))
-                                        "--merge-into" (str (io/file working-dir "my-app.json"))
-                                        "--output" (str (io/file working-dir "image.json")))))
+                                        "--base-image" (str (io/file target-dir "openjdk-alpine.json"))
+                                        "--merge-into" (str (io/file target-dir "my-app.json"))
+                                        "--output" (str (io/file target-dir "image.json")))))
 
                (is (= {:image
                        {:repository "nubank/my-app"
@@ -63,4 +65,27 @@
                        :service
                        {:name       "my-app"
                         :git-commit "07dccc801700cbe28e4a428e455b4627e0ab4ba9"}}
-                      (misc/read-json (io/file working-dir "image.json")))))))
+                      (misc/read-json (io/file target-dir "image.json")))))
+
+             (testing "containerizes the application"
+               (is (zero?
+                    (vessel/-main "containerize"
+                                  "--app-root" "/opt/my-app"
+                                  "--classpath" (classpath project-dir)
+                                  "--extra-path" (str (io/file project-dir "config/config.edn") ":/etc/my-app/config.edn")
+                                  "--main-class" "my-app.server"
+                                  "--manifest" (str (io/file target-dir "image.json"))
+                                  "--output" (str (io/file target-dir "my-app.tar"))
+                                  "--preserve-file-permissions"
+                                  "--source-path" (str (io/file project-dir "src"))
+                                  "--resource-path" (str (io/file project-dir "resources"))
+                                  "--verbose")))
+
+               (is (true? (misc/file-exists? (io/file target-dir "my-app.tar")))))
+
+             (testing "pushes the built image to the registry"
+               (is (zero?
+                    (vessel/-main "push"
+                                  "--tarball" (str (io/file target-dir "my-app.tar"))
+                                  "--allow-insecure-registries"
+                                  "--anonymous"))))))
