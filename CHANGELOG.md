82604e18797679383e05fad34b5bd7c176ebd5ac
Alan Ghelardi <alan.ghelardi@gmail.com>
2020-06-29 14:55:51 -0300

    Implement a custom mechanism for retrieving cached layers (#13)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,18 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+### ADDED
+- internal: `vessel.jib.containerizer` now supports an extended cache mechanism
+  that allows Vessel to inject custom cached layers into the container being
+  built. This feature aims to solve issues with the containerization process of compiled
+  Clojure files. Since the compilation isn't deterministic, it becomes
+  impossible to leverage regular cached layers and all layers end up being built every time ([#13](https://github.com/nubank/vessel/pull/13)).
+- Internal: add the dependency `com.google.cloud.tools/jib-build-plan`.
+
+### Changed
+- Improve significantly the design of `vessel.jib.containerizer` and fix flaky tests ([#13](https://github.com/nubank/vessel/pull/13)).
+- Internal: upgrade to ` com.google.cloud.tools/jib-core` 0.14.0.
+
 ## [0.2.128] - 2020-05-28
 
 ### Added
@@ -19,7 +31,11 @@ All notable changes to this project will be documented in this file.
 ### Added
 - Integration tests.
 - Workflow file to run tests automatically on Github Actions.
-- Normalize keys of the manifest.json file in order to avoid incongruities. Thus, Vessel can be employed  as a generic alternative to push tarballs to remote registries.
+
+### Fixed
+- Normalize keys of the manifest.json file in order to avoid
+  incongruities. Thus, Vessel can be employed as a generic alternative to push
+  tarballs to remote registries.
 
 ## [0.2.107] - 2020-03-09
 
@@ -34,23 +50,23 @@ All notable changes to this project will be documented in this file.
 ## [0.2.99] - 2020-03-02
 
 ### Added
-* Push command to upload a tarball to a registry.
+- Push command to upload a tarball to a registry.
 
 ### Changed
-* Internal: rename vessel.jib namespace to vessel.jib.containerizer and move
+- Internal: rename vessel.jib namespace to vessel.jib.containerizer and move
 functions that deal with common aspects of Jib API to the new
   vessel.jib.helpers namespace.
-* Upgrade Jib to version 0.13.0.
+- Upgrade Jib to version 0.13.0.
 
 ## [0.1.90] - 2020-02-20
 
 ### Added
-* Set the directory for caching base image and application layers to
+- Set the directory for caching base image and application layers to
   ~/.vessel-cache. Eventually, Vessel can take this directory as a parameter to
   allow a more fine-grained customization.
 
 ## [0.1.84] - 2020-02-18
 
 ### Added
-* Containerize, image and manifest commands (Vessel is in an alpha stage; the
+- Containerize, image and manifest commands (Vessel is in an alpha stage; the
   API is subject to changes).
--- test/unit/vessel/jib/containerizer_test.clj
+++ test/unit/vessel/jib/containerizer_test.clj
@@ -1,52 +1,121 @@
 (ns vessel.jib.containerizer-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
+            [matcher-combinators.matchers :as m]
             [matcher-combinators.test :refer [match?]]
             [vessel.jib.containerizer :as jib.containerizer]
             [vessel.misc :as misc]
             [vessel.test-helpers :refer [ensure-clean-test-dir]])
-  (:import org.apache.commons.vfs2.VFS))
+  (:import java.io.File
+           java.time.Instant
+           org.apache.commons.vfs2.VFS))
 
-(def tar-path "target/tests/containerizer-test/my-app.tar")
+(use-fixtures :once (ensure-clean-test-dir))
+
+(def tar-path-1 (io/file "target/tests/containerizer-test/my-app-1.tar"))
+
+(def tar-path-2 (io/file "target/tests/containerizer-test/my-app-2.tar"))
+
+(def cache-dir (misc/make-dir (misc/home-dir) ".vessel-cache"))
 
 (defn read-from-tarball
-  [^String file-name]
+  [^File tarball ^String file-name]
   (let [cwd      (str (.getCanonicalFile (io/file ".")))
-        tar-file (format "tar:%s/%s!/%s" cwd tar-path file-name)]
+        tar-file (format "tar:%s/%s!/%s" cwd tarball file-name)]
     (.. VFS getManager
         (resolveFile tar-file)
         getContent
         getInputStream)))
 
-(use-fixtures :once (ensure-clean-test-dir))
+;; Make layers deterministic.
+(def fixed-modification-time
+  (Instant/ofEpochMilli 0))
 
 (deftest containerize-test
   (testing "calls Google Jib and containerize the files in question"
-    (let [greeting-file (io/file "test/resources/greeting/greeting.txt")
-          layer-re      #"^[0-9a-f]{64}\.tar.gz$"]
+    (let [class-file (io/file "test/resources/containerization/fake_class.class")
+          resource   (io/file "test/resources/containerization/resource.edn")
+          build-result
+          (binding [misc/*verbose-logs* true]
+            (jib.containerizer/containerize #:image{:from   "openjdk@sha256:1fd5a77d82536c88486e526da26ae79b6cd8a14006eb3da3a25eb8d2d682ccd6"
+                                                    :target "nubank/my-app:v1"
+                                                    :layers
+                                                    [#:layer{:id      :source-paths
+                                                             :origin  :file-entries
+                                                             :entries [#:layer.entry{:source            (.getPath class-file)
+                                                                                     :target            "/opt/app/WEB-INF/classes/fake_class.class"
+                                                                                     :file-permissions  #{"OTHERS_READ"
+                                                                                                          "OWNER_WRITE"
+                                                                                                          "OWNER_READ"
+                                                                                                          "GROUP_READ"}
+                                                                                     :modification-time fixed-modification-time}]}
+                                                     #:layer{:id      :resource-paths
+                                                             :origin  :file-entries
+                                                             :entries [#:layer.entry{:source            (.getPath resource)
+                                                                                     :target            "/opt/app/WEB-INF/classes/resource.edn"
+                                                                                     :file-permissions  #{"OTHERS_READ"
+                                                                                                          "OWNER_WRITE"
+                                                                                                          "OWNER_READ"
+                                                                                                          "GROUP_READ"}
+                                                                                     :modification-time fixed-modification-time}]}]}
+                                            {:cache-dir cache-dir :tar-path tar-path-1}))]
+
+      (is (true? (misc/file-exists? (io/file tar-path-1)))
+          "the tarball has been created")
+
+      (is (match? [{:Config   "config.json"
+                    :RepoTags ["nubank/my-app:v1"]
+                    :Layers
+                    (m/in-any-order ["8e3ba11ec2a2b39ab372c60c16b421536e50e5ce64a0bc81765c2e38381bcff6.tar.gz",
+                                     "311ad0da45338842480bf25c6e6b7bb133b7b8cf709c3470db171ec370da5539.tar.gz",
+                                     "df312c74ce16f20eeb87b5640db9b1579a53534bd3e9f3de1e916fc62744bcf4.tar.gz",
+                                     "05edcc9e7f16871319590dccb2f9045f168a2cbdfab51b35b98693e57d42f7f7.tar.gz",
+                                     "86695d056ba72013e01b4c1363b63f63fdb90664adbbe043df1354c98c920906.tar.gz"])}]
+                  (misc/read-json (read-from-tarball tar-path-1 "manifest.json")))
+          "the image manifest has the expected layers")
+
+      (is (re-find #"^nubank/my-app@sha256:[a-f0-9]{64}$"
+                   (:image/reference build-result))
+          "the containerizer returned the correct image reference")
+
+      (is (re-find #"^sha256:[a-f0-9]{64}$"
+                   (:image/digest build-result))
+          "the containerizer returned a valid image digest")
+
+      (is (= {:source-paths
+              "sha256:05edcc9e7f16871319590dccb2f9045f168a2cbdfab51b35b98693e57d42f7f7"
+              :resource-paths
+              "sha256:86695d056ba72013e01b4c1363b63f63fdb90664adbbe043df1354c98c920906"}
+             (:application.layers/digests build-result))
+          "the build result contains a map of layer ids to their corresponding digests")))
+
+  (testing "builds a new image now retrieving the layer from the cache as per
+  specified in the build plan"
+    (let [resource (io/file "test/resources/containerization/resource.edn")]
       (binding [misc/*verbose-logs* true]
-        (jib.containerizer/containerize #:image{:from
-                                                #:image   {:repository "openjdk" :tag "sha256:1fd5a77d82536c88486e526da26ae79b6cd8a14006eb3da3a25eb8d2d682ccd6"}
-                                                :name
-                                                #:image   {:repository "nubank/my-app" :tag "v1"}
+        (jib.containerizer/containerize #:image{:from   "openjdk@sha256:1fd5a77d82536c88486e526da26ae79b6cd8a14006eb3da3a25eb8d2d682ccd6"
+                                                :target "nubank/my-app:v1"
                                                 :layers
-                                                [#:image.layer{:name    "resources"
-                                                               :entries [#:layer.entry{:source            (.getPath greeting-file)
-                                                                                       :target            "/opt/app/WEB-INF/classes/greeting.txt"
-                                                                                       :file-permissions  #{"OTHERS_READ"
-                                                                                                            "OWNER_WRITE"
-                                                                                                            "OWNER_READ"
-                                                                                                            "GROUP_READ"}
-                                                                                       :modification-time (misc/last-modified-time greeting-file)}]}]
-                                                :tar-path tar-path}))
+                                                [#:layer{:id     :source-paths
+                                                         :origin :extended-cache
+                                                         :digest "sha256:05edcc9e7f16871319590dccb2f9045f168a2cbdfab51b35b98693e57d42f7f7"}
+                                                 #:layer{:id      :resource-paths
+                                                         :origin  :file-entries
+                                                         :entries [#:layer.entry{:source            (.getPath resource)
+                                                                                 :target            "/opt/app/WEB-INF/classes/resource.edn"
+                                                                                 :modification-time fixed-modification-time}]}]}
+                                        {:cache-dir cache-dir :tar-path tar-path-2}))
 
-      (is (true? (misc/file-exists? (io/file tar-path))))
+      (is (true? (misc/file-exists? (io/file tar-path-2)))
+          "the tarball has been created")
 
-      (is (match? [{:config   "config.json"
-                    :repoTags ["nubank/my-app:v1"]
-                    :layers
-                    [layer-re
-                     layer-re
-                     layer-re
-                     layer-re]}]
-                  (misc/read-json (read-from-tarball "manifest.json")))))))
+      (is (match? [{:Config   "config.json"
+                    :RepoTags ["nubank/my-app:v1"]
+                    :Layers
+                    (m/in-any-order ["8e3ba11ec2a2b39ab372c60c16b421536e50e5ce64a0bc81765c2e38381bcff6.tar.gz",
+                                     "311ad0da45338842480bf25c6e6b7bb133b7b8cf709c3470db171ec370da5539.tar.gz",
+                                     "df312c74ce16f20eeb87b5640db9b1579a53534bd3e9f3de1e916fc62744bcf4.tar.gz",
+                                     "05edcc9e7f16871319590dccb2f9045f168a2cbdfab51b35b98693e57d42f7f7.tar.gz",
+                                     "86695d056ba72013e01b4c1363b63f63fdb90664adbbe043df1354c98c920906.tar.gz"])}]
+                  (misc/read-json (read-from-tarball tar-path-2 "manifest.json")))
+          "the image manifest has the same layers as the first one"))))
